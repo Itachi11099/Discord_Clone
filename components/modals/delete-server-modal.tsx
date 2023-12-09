455499e0a1eef1846c26812296a8fc5df41c2431
Vishvam Joshi <vishvamjoshi11099@gmail.com>
2023-12-09 17:40:50 +0530

    CHORE: implemented delete server functionality

---
+++ components/modals/delete-server-modal.tsx
@@ -0,0 +1,74 @@
+"use client";
+import { useState } from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useModal } from "@/hooks/use-modal-store";
+import { Button } from "@/components/ui/button";
+
+export const DeleteServerModal = () => {
+  const { isOpen, onClose, type, data } = useModal();
+  const router = useRouter();
+
+  const isModalOpen = isOpen && type === "deleteServer";
+  const { server } = data;
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+      await axios.delete(`/api/servers/${server?.id}`);
+      onClose();
+      // router.refresh();
+      router.push("/");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <Dialog open={isModalOpen} onOpenChange={onClose}>
+      <DialogContent>
+        <DialogHeader className="pt-8 px-6">
+          <DialogTitle className="text-2xl text-center font-bold">
+            Delete Server
+          </DialogTitle>
+          <DialogDescription className="text-center text-zinc-500 dark:text-zinc-100">
+            Are you sure you want to do this?
+            <br />
+            <span className="text-indigo-500 font-semibold">
+              {server?.name}
+            </span>{" "}
+            will be permanently deleted.
+          </DialogDescription>
+        </DialogHeader>
+        <DialogFooter className="px-6 py-4">
+          <div className="flex items-center justify-between w-full">
+            <Button
+              variant="ghost"
+              className="hover:bg-zinc-500 bg-zinc-600"
+              disabled={isLoading}
+              onClick={onClose}
+            >
+              Cancel
+            </Button>
+            <Button variant="primary" disabled={isLoading} onClick={onClick}>
+              Confirm
+            </Button>
+          </div>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};

d8d08c118a73c2edce93d2fb47de963541bfd8a8
Vishvam Joshi <vishvamjoshi11099@gmail.com>
2023-12-13 01:44:13 +0530

    CHORE: implemented infinite scrolling using cursor API method

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -6,6 +6,7 @@ import { db } from "@/lib/db";
 
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
+import { ChatMessages } from "@/components/chat/chat-messages";
 interface ChannelIdPageProps {
   params: {
     serverId: string;
@@ -43,7 +44,20 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         serverId={channel.serverId}
         type="channel"
       />
-      <div className="flex-1"> Future Messages</div>
+      <ChatMessages
+        member={member}
+        chatId={channel.id}
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/messages"
+        socketUrl="/api/socket/messages"
+        socketQuery={{
+          channelId: channel.id,
+          serverIdL: channel.serverId,
+        }}
+        paramKey="channelId"
+        paramValue={channel.id}
+      />
       <ChatInput
         name={channel.name}
         type="channel"
